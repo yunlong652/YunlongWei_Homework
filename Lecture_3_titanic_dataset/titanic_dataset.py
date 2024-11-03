d7eef836dc5e9638b46cd5a43baefed490a3e369
yunlong <yunlongwei777@gmail.com>
2024-11-03 19:20:48 +0300

    .

--- Lecture_3_titanic_dataset/titanic_dataset.py
+++ Lecture_3_titanic_dataset/titanic_dataset.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from skimpy import clean_columns
-
+#from skimpy import clean_columns
+from skimpy import skim
 
 df = pd.read_csv('/Users/apple/Documents/GitHub/YunlongWei_Homework/Lecture_3_titanic_dataset/titanic.csv')
 
