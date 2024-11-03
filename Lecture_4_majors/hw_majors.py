87633cba0fa3b97e69a8940c58bf02367ee17339
yunlong <yunlongwei777@gmail.com>
2024-11-03 23:03:13 +0300

    .py

---
+++ Lecture_4_majors/hw_majors.py
@@ -0,0 +1,5 @@
+'''Which major has the lowest unemployment rate?
+Which major has the highest percentage of women?
+How do the distributions of median income compare across major categories?
+Do women tend to choose majors with lower or higher earnings?'''
+
