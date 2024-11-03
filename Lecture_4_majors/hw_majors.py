73963a579fd8f853079ff4e8f80e053ff2cfb428
yunlong <yunlongwei777@gmail.com>
2024-11-03 23:23:03 +0300

    Update hw_majors.py

--- Lecture_4_majors/hw_majors.py
+++ Lecture_4_majors/hw_majors.py
@@ -3,3 +3,7 @@ Which major has the highest percentage of women?
 How do the distributions of median income compare across major categories?
 Do women tend to choose majors with lower or higher earnings?'''
 
+import pandas as pd
+
+
+
