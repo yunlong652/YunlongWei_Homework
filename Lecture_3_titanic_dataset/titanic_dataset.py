0c6a466e321bd80f991954ae373371beb3428b7a
yunlong <yunlongwei777@gmail.com>
2024-11-03 23:13:08 +0300

    Update titanic_dataset.py

--- Lecture_3_titanic_dataset/titanic_dataset.py
+++ Lecture_3_titanic_dataset/titanic_dataset.py
@@ -2,20 +2,38 @@ import pandas as pd
 from skimpy import skim
 import matplotlib.pyplot as plt
 
+# Load the Titanic dataset
 df = pd.read_csv('/Users/apple/Documents/GitHub/YunlongWei_Homework/Lecture_3_titanic_dataset/titanic.csv')
 
+# Display a summary of the dataset
 skim(df) 
-#print(df.head())
-#print(df.info())
-#print(df.describe())
 
+# to view the first few rows, info, and descriptive statistics
+print(df.head())
+print(df.info())
+print(df.describe()) 
+
+# Count the number of survivors and non-survivors
 sur_counts = df['Survived'].value_counts()
+
+# Plot the survival counts as a bar chart
 sur_counts.plot(kind='bar')
-plt.xticks(ticks=[0, 1], labels=['no', 'yes'], rotation = 0)
+plt.xticks(ticks=[0, 1], labels=['No', 'Yes'], rotation=0)
+plt.title('Survival Counts')
+plt.xlabel('Survived')
+plt.ylabel('Count')
 plt.show()
 
+# Group data by gender and survival status, then count occurrences
 gen_sur = df.groupby(['sex', 'Survived']).size().unstack()
+
+# Plot the gender survival counts as a stacked bar chart
 gen_sur.plot(kind='bar', stacked=True)
-plt.xticks(rotation = 0)
+plt.xticks(rotation=0)
+plt.title('Survival by Gender')
+plt.xlabel('Gender')
+plt.ylabel('Count')
+plt.legend(title='Survived', labels=['No', 'Yes'])
 plt.show()
+
 
